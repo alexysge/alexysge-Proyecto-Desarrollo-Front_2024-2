a2aa533b09ae7a0f5a6bc953f1cf26c7e3341d55
Alexys Gomez Elizalde <41647329+alexysge@users.noreply.github.com>
2024-03-28 22:15:12 -0600

    P7 finalizada
    Me falto un archivo

--- src/app/module/product.module.ts
+++ src/app/module/product.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 
